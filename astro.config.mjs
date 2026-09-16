9b7b8f74a30435cc82a107ec1e9f61d4cee2f8e7
FernandoMejia19 <fercholkm19@gmail.com>
2026-09-16 12:33:48 -0500

    Correccione en UI y formulario

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-const GITHUB_USERNAME = 'gabusleon';
-const GITHUB_REPOSITORY = 'kivanaApp-webpage';
+const GITHUB_USERNAME = 'FernandoMejia19';
+const GITHUB_REPOSITORY = 'kivana-app-practica-prepro';
 
 const esPaginaUsuario =
   GITHUB_REPOSITORY.toLowerCase() ===
